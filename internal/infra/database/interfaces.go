7bed03c2d1f3d90b781cd5eb904881a5282573f9
vitorpcruz <vitor-pcruz@hotmail.com>
2024-05-03 19:43:33 -0300

    creating product repo

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserRepositoryInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
+
+type ProductRepositoryInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
---
+++ internal/infra/database/product-repository.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"github.com/vitorpcruz/goexpert/9-APIS/internal/entity"
+	"gorm.io/gorm"
+)
+
+type ProductRepository struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *ProductRepository {
+	return &ProductRepository{DB: db}
+}
+
+func (p *ProductRepository) Create(product *entity.Product) error {
+	return p.DB.Create(product).Error
+}
+
+func (p *ProductRepository) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+
+	sort = setSort(sort)
+
+	if page != 0 && limit != 0 {
+		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("created_at " + sort).Find(&products).Error
+	}
+	return products, err
+}
+
+func setSort(sort string) string {
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+	return sort
+}
+
+func (p *ProductRepository) FindByID(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := p.DB.First(&product, "id = ?", id).Error
+	return &product, err
+}
+
+func (p *ProductRepository) Update(product *entity.Product) error {
+	if _, err := p.FindByID(product.ID.String()); err != nil {
+		return err
+	}
+	return p.DB.Save(product).Error
+}
+
+func (p *ProductRepository) Delete(id string) error {
+	product, err := p.FindByID(id)
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(product).Error
+}
